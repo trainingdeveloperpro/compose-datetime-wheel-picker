caa88dcd29208db212366f3f796c955648312dad
Linh Van Nguyen <linhvnguyen9@gmail.com>
2025-01-16 21:54:55 +0700

    Adding a few locales for DateOrder

--- datetime-wheel-picker/src/commonMain/kotlin/dev/darkokoa/datetimewheelpicker/core/format/DateOrder.kt
+++ datetime-wheel-picker/src/commonMain/kotlin/dev/darkokoa/datetimewheelpicker/core/format/DateOrder.kt
@@ -7,12 +7,12 @@ enum class DateOrder(val fields: List<DateField>) {
   MDY(listOf(DateField.MONTH, DateField.DAY, DateField.YEAR)),   // USA
   YMD(listOf(DateField.YEAR, DateField.MONTH, DateField.DAY));   // East Asian countries, ISO
 
-
+  // https://en.wikipedia.org/wiki/List_of_date_formats_by_country
   companion object {
     fun match(locale: Locale): DateOrder {
       return when {
-        locale.language == "en" && locale.region == "US" -> MDY
-        locale.language in listOf("zh", "ja", "ko") -> YMD
+        locale.language == "en" && locale.region in listOf("US", "PH", "FM", "PW", "CA", "KY", "GU", "AS", "PR", "VI", "MP", "MH", "BS", "BZ", "LR", "TG", "KE", "GH")  -> MDY
+        locale.language in listOf("zh", "ja", "ko", "hu", "mn", "lt", "dz") -> YMD
         else -> DMY
       }
     }
